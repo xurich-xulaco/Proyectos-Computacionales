ee997a654e35597c44654d32cb1807a4a781d8a5
TomasTapia302 <a310962@alumnos.uaslp.mx>
2024-12-13 01:20:51 -0600

    completo hasta donde tengo

--- Cronometraje_Carreras_Deportivas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cronometraje_Carreras_Deportivas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
